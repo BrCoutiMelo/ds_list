57fc400f2ee662384ed37101fe7bce68a3fea14f
Breno <brenomelocouti1@gmail.com>
2025-01-23 12:27:21 -0300

    Database seed update

---
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -0,0 +1,59 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+@Embeddable
+public class BelongingPK {
+    /*
+    * Classe criada para auxiliar a Classe Belonging
+    * Para referenciar as Chaves Primarias das Classes Game e Gamelist
+    * */
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList gameList;
+
+    public BelongingPK(){
+
+    }
+
+    public BelongingPK(Game game, GameList gameList) {
+        this.game = game;
+        this.gameList = gameList;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public GameList getGameList() {
+        return gameList;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public void setGameList(GameList gameList) {
+        this.gameList = gameList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BelongingPK that = (BelongingPK) o;
+        return Objects.equals(game, that.game) && Objects.equals(gameList, that.gameList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, gameList);
+    }
+}
